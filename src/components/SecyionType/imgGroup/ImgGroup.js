23ef4d2905e6ccb15223c84672b9ccfcf09e9b93
khderalromi <www.khderalromi999@gmail.com>
2024-03-13 13:04:33 +0300

    cars

--- src/components/SecyionType/imgGroup/ImgGroup.js
+++ src/components/SecyionType/imgGroup/ImgGroup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {Component} from "react";
 import classes from './ImgGroup.css'
 import img1 from '../../../assests/images/Rectangle1.png';
 import img2 from '../../../assests/images/Rectangle2.png';
@@ -10,34 +10,70 @@ import {  faCaretRight,faCaretLeft } from
 
 
 
-const images=[  {img:img1},
+
+class ImgGroup extends Component{
+    state={
+        index:3,
+        images:[  
+                {img:img1},
                 {img:img2, name:"BMW"},
                 {img:img3,name:"Bugotti" },
                 {img:img4}
-            ];
+                ]
+    }
 
-const ImgGroup= (props)=>{
-    return(
-        <div className={classes.ImgGroup}>
-            <FontAwesomeIcon className={classes.icon} icon={faCaretLeft}/>
+      
+    previous=()=>{
+        {if(this.state.index >0 && this.state.index <=3){
+            const updateIndex= this.state.index-1;
+            return (this.setState({index: updateIndex}))
+           }
+        }   
+     }
+
+     next=()=>{
+       if(this.state.index < 3){
+        const updateIndex= this.state.index+1;
+        return (this.setState({index: updateIndex}))
+       }
+        
+        
+ }
+     HandleImageClick=(myindex)=>{
+            this.setState({index:myindex})
+        }
+
+    render(){
+        return(
+            <div className={classes.ImgGroup}>
+            <FontAwesomeIcon
+             className={classes.icon} 
+             icon={faCaretLeft}
+             onClick={()=>{this.previous()}}
+             />
 
             <div className={classes.images}>
-            {images.map((image)=>{
+            {[0,1,2,3].map((index)=>{
                 return(
                     <div>
-                        <img src={image.img} ></img>
+                        <img src={this.state.images[index].img }
+                        className={this.state.index===index ? classes.selected_slide : classes.slide}
+                        onClick={()=>{return (this.HandleImageClick(index))}} ></img>
                     </div>
                     
                 )
             })}  
             </div>
             
-            <FontAwesomeIcon className={classes.icon} icon={faCaretRight}/>
+            <FontAwesomeIcon 
+                className={classes.icon} 
+                icon={faCaretRight}
+                onClick={()=>{this.next()}}/>
         </div>
         
-        
-    )
-
+        )
+    }
 }
+
 
 export default ImgGroup;
